ba19cf1839007a82d007b88be122acb1a6bdf5fb
Joshua Sanders <ninjosh2@gmail.com>
2020-01-12 23:32:29 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,4 @@
 class Person
-  #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complecxion, :t_shirt_size, :pant_length, :pant_width 
+  
 end
